31a25e33988ba462b5bc7fea989d52a7cec55b16
HiuuKun <hieutraihvt@gmail.com>
2023-09-30 19:10:09 +0700

    Add exact path

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Home from './Home';
 import Blog from './Blog';
 import FullBlog from './FullBlog';
 import Contact from './Contact';
-import logo from './team-logo.png';
+
 function App() {
   return (
     <div >
@@ -16,7 +16,7 @@ function App() {
       
       <header className='content' >
         <Routes>
-          <Route path = "/" element={<Home/>}/>
+          <Route exact path = "/" element={<Home/>}/>
           <Route path = "/contact" element={<Contact/>}/>
           <Route path = "/blog" element={<Blog/>}/>
           <Route path="/full-blog/:id" element={<FullBlog/>} /> 
