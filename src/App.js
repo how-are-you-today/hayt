877c1ce69b3e06d9cf302488074df5ac0ac73f94
HiuuKun <74101518+HiuuKun@users.noreply.github.com>
2023-09-30 19:23:23 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
       
       <header className='content' >
         <Routes>
-          <Route exact path = "/" element={<Home/>}/>
+          <Route path = "/" element={<Home/>}/>
           <Route path = "/contact" element={<Contact/>}/>
           <Route path = "/blog" element={<Blog/>}/>
           <Route path="/full-blog/:id" element={<FullBlog/>} /> 
